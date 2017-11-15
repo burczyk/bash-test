f668f1c94ed247f098284532b2d6dece9a4a58ab
Kamil Burczyk <kamil@MacBook-Pro-Kamil.local>
2017-11-15 20:50:08 +0100

    Updated files

---
+++ commit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+repository_name=bash-test
+repository_path=git@github.com:burczyk/$repository_name.git
+
+function replace_and_commit {
+  cp commit.sh $repository_name
+  pwd
+  cd $repository_name
+  git add .
+  git commit -m "Updated files"
+  git push origin master
+}
+
+if [ -d $repository_name ]
+then
+  echo $repository_name exists - pulling newest data
+  cd $repository_name
+  git pull origin master
+  cd ..
+  replace_and_commit
+else
+  echo $repository_name does not exist - cloning
+  git clone $repository_path
+  replace_and_commit
+fi
